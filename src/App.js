594f8ff93765a4992cc4720451c81fa39caba063
fygure <92011471+fygure@users.noreply.github.com>
2023-09-07 22:49:46 -0500

    added material UI dependency and updated metadata

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Button variant="contained">HELLO WORLD</Button>
+
       <h2>Testing Testing</h2>
     </div>
   );
