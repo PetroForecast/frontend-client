87a78e867a2776054b57bcaa1d684a5dca9911f9
Kurmanbek Bazarov <93934476+DrNyktersten@users.noreply.github.com>
2023-09-22 12:50:41 -0500

    navbar text ui fails to work consistently

--- src/components/DefaultAppBar.jsx
+++ src/components/DefaultAppBar.jsx
@@ -1,17 +1,121 @@
+// import * as React from 'react';
+// import AppBar from '@mui/material/AppBar';
+// import Box from '@mui/material/Box';
+// import Toolbar from '@mui/material/Toolbar';
+// import Typography from '@mui/material/Typography';
+// import Menu from '@mui/material/Menu';
+// import OilBarrelIcon from '@mui/icons-material/OilBarrel';
+// import Container from '@mui/material/Container';
+// import Button from '@mui/material/Button';
+// import MenuItem from '@mui/material/MenuItem';
+// const pages = ['Products', 'Pricing', 'Blog', 'Demo'];
+
+// export default function DefaultAppBar(props) {
+//   return (
+//     <AppBar position="static">
+//       <Container maxWidth="false">
+//         <Toolbar disableGutters>
+//           <OilBarrelIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+//           <Typography
+//             variant="h6"
+//             noWrap
+//             component="a"
+//             href="/"
+//             sx={{
+//               mr: 2,
+//               display: { xs: 'none', md: 'flex' },
+//               fontFamily: 'monospace',
+//               fontWeight: 700,
+//               letterSpacing: '.05rem',
+//               color: 'inherit',
+//               textDecoration: 'none',
+//             }}
+//           >
+//             PETROFORECAST
+//           </Typography>
+//           <OilBarrelIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+//           <Typography
+//             variant="h5"
+//             noWrap
+//             component="a"
+//             href="/"
+//             sx={{
+//               mr: 2,
+//               display: { xs: 'flex', md: 'none' },
+//               flexGrow: 1,
+//               fontFamily: 'monospace',
+//               fontWeight: 700,
+//               letterSpacing: '.05rem',
+//               color: 'inherit',
+//               textDecoration: 'none',
+//             }}
+//           >
+//             PETROFORECAST
+//           </Typography>
+//           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+//             {pages.map((page) => (
+//               <Button
+//                 key={page}
+//                 sx={{ my: 2, color: 'white', display: 'block', '&:hover': {backgroundColor: '#22c55e',}, }} 
+//               >
+//                 <a href={`/${page.toLowerCase()}`} style={{ textDecoration: 'none', color: 'white', }}>{page.toLowerCase()}</a>
+//               </Button>
+//             ))}
+//           </Box>
+
+//           <Box sx={{
+//             flexGrow: 0,
+//           }}>
+//             <Button
+//               variant="outlined"
+//               color="inherit"
+//               onClick={props.onLogin}
+//               sx={{
+//                 '&:hover': {
+//                   backgroundColor: 'rgba(0, 191, 255, 0.3)',
+//                 },
+//               }}
+//             >
+//               Login
+//             </Button>
+//             <Button
+//               variant="outlined"
+//               color="inherit"
+//               onClick={props.onRegistration}
+//               sx={{
+//                 ml: 2,
+//                 '&:hover': {
+//                   backgroundColor: 'rgba(0, 191, 255, 0.3)',
+//                 },
+//               }}
+//             >
+//               Register
+//             </Button>
+//           </Box>
+//         </Toolbar>
+//       </Container>
+//     </AppBar>
+//   );
+// }
+
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
 import OilBarrelIcon from '@mui/icons-material/OilBarrel';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
+
 const pages = ['Products', 'Pricing', 'Blog', 'Demo'];
 
-
 export default function DefaultAppBar(props) {
+  const [activeLink, setActiveLink] = React.useState(''); // State to track the active link
+
+  const handleLinkClick = (page) => {
+    console.log('Clicked:', page.toLowerCase());
+    setActiveLink(page.toLowerCase()); // Set the active link state
+  };
 
   return (
     <AppBar position="static">
@@ -58,16 +162,23 @@ export default function DefaultAppBar(props) {
             {pages.map((page) => (
               <Button
                 key={page}
-                sx={{ my: 2, color: 'white', display: 'block', '&:hover': {backgroundColor: '#22c55e',}, }} 
+                sx={{
+                  my: 2,
+                  color: 'white',
+                  display: 'block',
+                  '&:hover': { backgroundColor: '#22c55e' },
+                  backgroundColor: activeLink === page.toLowerCase() ? 'orange' : 'inherit', // Set the background color based on activeLink state
+                }}
+                onClick={() => handleLinkClick(page)}
               >
-                <a href={`/${page.toLowerCase()}`} style={{ textDecoration: 'none', color: 'white', }}>{page.toLowerCase()}</a>
+                <a href={`/${page.toLowerCase()}`} style={{ textDecoration: 'none', color: 'white' }}>
+                  {page.toLowerCase()}
+                </a>
               </Button>
             ))}
           </Box>
 
-          <Box sx={{
-            flexGrow: 0,
-          }}>
+          <Box sx={{ flexGrow: 0 }}>
             <Button
               variant="outlined"
               color="inherit"
