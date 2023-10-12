e5cc4722acbedf9f13917b5300c313792e81eb00
fygure <92011471+fygure@users.noreply.github.com>
2023-10-12 01:30:28 -0500

    added update profile button to user Profile page

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Container, Typography, Paper, Grid } from '@mui/material';
+import { Container, Typography, Paper, Grid, Button } from '@mui/material';
 
 function UserProfile() {
-  //FIXME real logic
-  // Retrieve user data from local storage
   const storedUser = localStorage.getItem('currentUser');
   const user = storedUser ? JSON.parse(storedUser) : null;
+  console.log(user);
 
   const borderStyle = {
     border: '1px solid',
@@ -17,6 +16,9 @@ function UserProfile() {
       <Paper elevation={3} sx={{ p: 4, mt: 2 }}>
         <Typography variant="h4" gutterBottom>
           User Profile
+          <Button variant="contained" color="primary" style={{ marginLeft: '10px', marginBottom: '5px' }}>
+            Update Profile
+          </Button>
         </Typography>
         {user ? (
           <div>
@@ -24,37 +26,37 @@ function UserProfile() {
               <Grid item xs={12}>
                 <Paper elevation={0} style={borderStyle}>
                   <Typography variant="h6">Full Name:</Typography>
-                  <Typography variant="body1">{user.profile ? user.profile.name : ''}</Typography>
+                  <Typography variant="body1">{user.fullName}</Typography>
                 </Paper>
               </Grid>
               <Grid item xs={6}>
                 <Paper elevation={0} style={borderStyle}>
                   <Typography variant="h6">Address 1:</Typography>
-                  <Typography variant="body1">{user.profile ? user.profile.address1 : ''}</Typography>
+                  <Typography variant="body1">{user.address1}</Typography>
                 </Paper>
               </Grid>
               <Grid item xs={6}>
                 <Paper elevation={0} style={borderStyle}>
                   <Typography variant="h6">Address 2:</Typography>
-                  <Typography variant="body1">{user.profile ? user.profile.address2 : ''}</Typography>
+                  <Typography variant="body1">{user.address2 = user.address2 ? user.address2 : 'N/A'}</Typography>
                 </Paper>
               </Grid>
               <Grid item xs={6}>
                 <Paper elevation={0} style={borderStyle}>
                   <Typography variant="h6">City:</Typography>
-                  <Typography variant="body1">{user.profile ? user.profile.city : ''}</Typography>
+                  <Typography variant="body1">{user.city}</Typography>
                 </Paper>
               </Grid>
               <Grid item xs={3}>
                 <Paper elevation={0} style={borderStyle}>
                   <Typography variant="h6">State:</Typography>
-                  <Typography variant="body1">{user.profile ? user.profile.state : ''}</Typography>
+                  <Typography variant="body1">{user.state}</Typography>
                 </Paper>
               </Grid>
               <Grid item xs={3}>
                 <Paper elevation={0} style={borderStyle}>
                   <Typography variant="h6">Zipcode:</Typography>
-                  <Typography variant="body1">{user.profile ? user.profile.zipcode : ''}</Typography>
+                  <Typography variant="body1">{user.zipcode}</Typography>
                 </Paper>
               </Grid>
             </Grid>
