49a5c0a797f4b88ac7e708d04705fce3fa15a377
jassingh1313 <jassingh2911@gmail.com>
2023-10-18 18:11:03 -0500

    Using image from S3 bucket

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,75 +1,75 @@
-import React from 'react';
-import ceo from './images/main_image_4353.webp'; 
-import states from './images/states.webp'; 
-import data from './images/data.webp'; 
-import attempt from './images/try.webp'; 
-// import reactlogo from '../../public/logo192.png'; 
+import React from "react";
+import ceo from "./images/main_image_4353.webp";
+import states from "./images/states.webp";
+import data from "./images/data.webp";
+import attempt from "./images/try.webp";
+// import reactlogo from '../../public/logo192.png';
 
 function ProductsPage() {
   const ceo_main = {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: '5px',
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "5px",
   };
 
   const ceo_image = {
-    width: '60rem',
-    height: '30rem',
+    width: "60rem",
+    height: "30rem",
   };
 
   const ceo_text = {
-    fontSize: 'larger',
-    maxWidth: '550px',
-    overflow: 'hidden',
+    fontSize: "larger",
+    maxWidth: "550px",
+    overflow: "hidden",
   };
 
   const ceo_h1 = {
-    fontFamily: 'Arial sans-serif',
-    fontSize: '26px',
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontFamily: "Arial sans-serif",
+    fontSize: "26px",
+    fontWeight: "bold",
+    textAlign: "center",
   };
 
   const boxes = {
-    height: '100%',
-    margin: '0',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    height: "100%",
+    margin: "0",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   };
 
   const container = {
-    width: '300px',
-    height: '300px',
-    position: 'relative',
-    margin: '0 20px', /* Add margin for spacing between elements */
-    overflow: 'hidden', // Hide any content that overflows the container
-    borderRadius: '20px', // Rounded corners
+    width: "300px",
+    height: "300px",
+    position: "relative",
+    margin: "0 20px" /* Add margin for spacing between elements */,
+    overflow: "hidden", // Hide any content that overflows the container
+    borderRadius: "20px", // Rounded corners
   };
 
   const imageWrapper = {
-    width: '100%',
-    height: '100%',
-    position: 'relative',
+    width: "100%",
+    height: "100%",
+    position: "relative",
   };
 
   const background_image = {
-    width: '100%',
-    height: '100%',
-    objectFit: 'cover', // Cover the container
-    backgroundColor: 'blue',
+    width: "100%",
+    height: "100%",
+    objectFit: "cover", // Cover the container
+    backgroundColor: "blue",
   };
 
   const text = {
-    position: 'absolute',
-    bottom: '0',
-    left: '0',
-    right: '0',
-    backgroundColor: 'white',
-    padding: '10px',
-    textAlign: 'center',
+    position: "absolute",
+    bottom: "0",
+    left: "0",
+    right: "0",
+    backgroundColor: "white",
+    padding: "10px",
+    textAlign: "center",
   };
 
   return (
@@ -77,7 +77,7 @@ function ProductsPage() {
       <div className="Main-image-text" style={ceo_main}>
         <img
           alt="This CEO and entrepreneur are working on their laptops"
-          src={ceo}
+          src="https://petroforecastimages.s3.amazonaws.com/images/main_image_4353.webp"
           style={ceo_image}
         />
         <div style={ceo_text}>
