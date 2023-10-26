159a9673410453693eb0ebeb470502f60f3f16df
jassingh1313 <jassingh2911@gmail.com>
2023-10-26 16:42:09 -0500

    Fixed fetching fuel quotes for table

--- src/components/FuelQuoteHistoryTable.jsx
+++ src/components/FuelQuoteHistoryTable.jsx
@@ -8,19 +8,25 @@ import { Paper } from "@mui/material";
 
 const columns = [
   //column definition for table
+  { field: "quoteId", headerName: "Quote Id", width: 200 },
   { field: "gallonsRequested", headerName: "Gallons Requested", width: 200 },
   { field: "deliveryAddress", headerName: "Delivery Address", width: 200 },
   { field: "deliveryDate", headerName: "Delivery Date", width: 200 },
   {
-    field: "pricePerGallon",
+    field: "suggestedPricePerGallon",
     headerName: "Suggested Price per Gallon",
     width: 200,
   },
-  { field: "amountDue", headerName: "Total Amount Due", width: 200 },
+  { field: "totalAmountDue", headerName: "Total Amount Due", width: 200 },
 ];
 
+export default function FuelQuoteHistoryTable({ latestQuotes }) {
+  const rows = latestQuotes.map((row) => ({
+    id: row.quoteId,
+    ...row,
+    deliveryDate: row.deliveryDate.split("T")[0],
+  }));
 
-export default function FuelQuoteHistoryTable({latestQuotes}) {
   return (
     <Paper elevation={4} sx={{ p: 2 }}>
       <Box style={{ height: "auto", overflow: "auto" }}>
@@ -28,7 +34,7 @@ export default function FuelQuoteHistoryTable({latestQuotes}) {
           Fuel Quote History
         </Typography>
         <DataGrid
-          rows={latestQuotes}
+          rows={rows}
           columns={columns}
           initialState={{
             //sets the defualt look of table
