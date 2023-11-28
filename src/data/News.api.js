f809c0a70fea3a83659655c70e70e20138d8b71e
jike <jessicaike123@gmail.com>
2023-11-27 20:56:04 -0600

    Fixed error with api, removed it from blog page.

--- src/data/News.api.js
+++ src/data/News.api.js
@@ -3,7 +3,7 @@ const API_KEY = "930ceaf297544f81b46b63bf4d5c1830";
 
 export const getNews = async (itemBody) => {
   try { //API returns 6 top-headlines related to gas from the us
-    const gasResponse = await fetch('https://newsapi.org/v2/top-headlines?category=business&country=us&q=gas&pageSize=3&apiKey='+API_KEY);
+    const gasResponse = await fetch('https://newsapi.org/v2/top-headlines?category=business&country=us&q=gas&pageSize=3&apiKe='+API_KEY);
     if (!gasResponse.ok) {
         throw new Error(`Fetch error: ${gasResponse.status}`);
     }
